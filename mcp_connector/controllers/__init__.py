2f7bcb93adb46eb08706843fd76212bccab07654
Marcelo Santos <marcelo.net77@hotmail.com>
2026-02-28 00:40:02 +0000

    Revert "Build installable MCP Odoo addon scaffold"
    This reverts commit 652f3d7164f7f041527249691684e4c9dff336a9.

--- mcp_connector/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import rpc
-from . import webapp
--- mcp_connector/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import mcp_server
-from . import mcp_agent_tool
-from . import mcp_memory_context
-from . import mcp_activity_log
-from . import mcp_echart_dashboard
-from . import mcp_webapp
